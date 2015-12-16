7c2dd944975afa266085f8026c9bc0f43395445c
Alessandro Roncone <alecive87@gmail.com>
2015-12-16 12:30:11 -0500

    [ttt_board_sensor] Cleanup of board_state_sesing

--- ttt_board_sensor/src/board_state_sensing.cpp
+++ ttt_board_sensor/src/board_state_sensing.cpp
@@ -29,14 +29,25 @@ bool operator!=(ttt_board_sensor::ttt_board msg1,ttt_board_sensor::ttt_board msg
     return !(msg1==msg2);
 }
 
+struct color_range {
+    int min;
+    int max;
+};
+
+struct hsv_color {
+    color_range H;
+    color_range S;
+    color_range V;
+};
+
 class BoardState
 {
 private:
 
-    ros::NodeHandle nh_;
-    ros::Publisher board_pub_;
-    image_transport::ImageTransport it_;
-    image_transport::Subscriber image_sub_;
+    ros::NodeHandle node_handle;
+    ros::Publisher  board_publisher;
+    image_transport::ImageTransport image_transport;
+    image_transport::Subscriber     image_subscriber;
 
     std::string board_config;
 
@@ -44,8 +55,8 @@ private:
 
     double TOKEN_AREA_THRESHOLD;
 
-    int H_LOWER_RED, S_LOWER_RED, V_LOWER_RED, H_HIGHER_RED, S_HIGHER_RED, V_HIGHER_RED;
-    int H_LOWER_BLUE, S_LOWER_BLUE, V_LOWER_BLUE, H_HIGHER_BLUE, S_HIGHER_BLUE, V_HIGHER_BLUE;
+    hsv_color red_params;
+    hsv_color blue_params;
 
     double get_hsv_colored_area_in_cell(const cv::Mat& img, const t_Cell& cell, const cv::Scalar& lower_values, const cv::Scalar& higher_values) const
     {
@@ -84,51 +95,51 @@ private:
 
 public:
     BoardState()
-        : it_(nh_)
+        : image_transport(node_handle)
     {
-        this->image_sub_ = this->it_.subscribe("image_in", 1, &BoardState::imageCb, this);
-        this->board_pub_ = this->nh_.advertise<ttt_board_sensor::ttt_board>("/new_board", 1);
-        ROS_ASSERT_MSG(this->board_pub_,"Empty publisher");
+        image_subscriber = image_transport.subscribe("image_in", 1, &BoardState::imageCb, this);
+        board_publisher  = node_handle.advertise<ttt_board_sensor::ttt_board>("/new_board", 1);
+        ROS_ASSERT_MSG(board_publisher,"Empty publisher");
 
         /* Reading cells definition data from the parameter server */
-        ROS_ASSERT_MSG(Cells::read_from_parameter_server(this->board,Cells::CELLS_DATA_PARAM_NAME),"No cell data to display!");
+        ROS_ASSERT_MSG(Cells::read_from_parameter_server(board,Cells::CELLS_DATA_PARAM_NAME),"No cell data to display!");
 
         /* Reading the segmentation thresholds for red tokens */
-        ROS_ASSERT_MSG(this->nh_.hasParam("h_low_red"),"No H LOW threshold for RED tokens!");
-        this->nh_.getParam("h_low_red",this->H_LOWER_RED);
-        ROS_ASSERT_MSG(this->nh_.hasParam("s_low_red"),"No S LOW threshold for RED tokens!");
-        this->nh_.getParam("s_low_red",this->S_LOWER_RED);
-        ROS_ASSERT_MSG(this->nh_.hasParam("v_low_red"),"No V LOW threshold for RED tokens!");
-        this->nh_.getParam("v_low_red",this->V_LOWER_RED);
-        ROS_ASSERT_MSG(this->nh_.hasParam("h_high_red"),"No H HIGH threshold for RED tokens!");
-        this->nh_.getParam("h_high_red",this->H_HIGHER_RED);
-        ROS_ASSERT_MSG(this->nh_.hasParam("s_high_red"),"No S HIGH threshold for RED tokens!");
-        this->nh_.getParam("s_high_red",this->S_HIGHER_RED);
-        ROS_ASSERT_MSG(this->nh_.hasParam("v_high_red"),"No V HIGH threshold for RED tokens!");
-        this->nh_.getParam("v_high_red",this->V_HIGHER_RED);
-        ROS_INFO_STREAM("Red tokens in H=[" << this->H_LOWER_RED<< ".." << this->H_HIGHER_RED <<
-                        "] S=[" << this->S_LOWER_RED << ".." << this->S_HIGHER_RED <<
-                        "] V=[" << this->V_LOWER_RED << ".." << this->V_HIGHER_RED<< "]");
+        ROS_ASSERT_MSG(node_handle.hasParam("h_low_red"),"No H LOW threshold for RED tokens!");
+        node_handle.getParam("h_low_red",red_params.H.min);
+        ROS_ASSERT_MSG(node_handle.hasParam("s_low_red"),"No S LOW threshold for RED tokens!");
+        node_handle.getParam("s_low_red",red_params.S.min);
+        ROS_ASSERT_MSG(node_handle.hasParam("v_low_red"),"No V LOW threshold for RED tokens!");
+        node_handle.getParam("v_low_red",red_params.V.min);
+        ROS_ASSERT_MSG(node_handle.hasParam("h_high_red"),"No H HIGH threshold for RED tokens!");
+        node_handle.getParam("h_high_red",red_params.H.max);
+        ROS_ASSERT_MSG(node_handle.hasParam("s_high_red"),"No S HIGH threshold for RED tokens!");
+        node_handle.getParam("s_high_red",red_params.S.max);
+        ROS_ASSERT_MSG(node_handle.hasParam("v_high_red"),"No V HIGH threshold for RED tokens!");
+        node_handle.getParam("v_high_red",red_params.V.max);
+        ROS_INFO_STREAM("Red tokens in H=[" << red_params.H.min<< ".." << red_params.H.max <<
+                        "] S=[" << red_params.S.min << ".." << red_params.S.max <<
+                        "] V=[" << red_params.V.min << ".." << red_params.V.max<< "]");
 
         /* Reading the segmentation thresholds for blue tokens */
-        ROS_ASSERT_MSG(this->nh_.hasParam("h_low_blue"),"No H LOW threshold for BLUE tokens!");
-        this->nh_.getParam("h_low_blue",this->H_LOWER_BLUE);
-        ROS_ASSERT_MSG(this->nh_.hasParam("s_low_blue"),"No S LOW threshold for BLUE tokens!");
-        this->nh_.getParam("s_low_blue",this->S_LOWER_BLUE);
-        ROS_ASSERT_MSG(this->nh_.hasParam("v_low_blue"),"No V LOW threshold for BLUE tokens!");
-        this->nh_.getParam("v_low_blue",this->V_LOWER_BLUE);
-        ROS_ASSERT_MSG(this->nh_.hasParam("h_high_blue"),"No H HIGH threshold for BLUE tokens!");
-        this->nh_.getParam("h_high_blue",this->H_HIGHER_BLUE);
-        ROS_ASSERT_MSG(this->nh_.hasParam("s_high_blue"),"No S HIGH threshold for BLUE tokens!");
-        this->nh_.getParam("s_high_blue",this->S_HIGHER_BLUE);
-        ROS_ASSERT_MSG(this->nh_.hasParam("v_high_blue"),"No V HIGH threshold for BLUE tokens!");
-        this->nh_.getParam("v_high_blue",this->V_HIGHER_BLUE);
-        ROS_INFO_STREAM("Blue tokens in H=[" << this->H_LOWER_BLUE<< ".." << this->H_HIGHER_BLUE <<
-                        "] S=[" << this->S_LOWER_BLUE << ".." << this->S_HIGHER_BLUE <<
-                        "] V=[" << this->V_LOWER_BLUE << ".." << this->V_HIGHER_BLUE<< "]");
+        ROS_ASSERT_MSG(node_handle.hasParam("h_low_blue"),"No H LOW threshold for BLUE tokens!");
+        node_handle.getParam("h_low_blue",blue_params.H.min);
+        ROS_ASSERT_MSG(node_handle.hasParam("s_low_blue"),"No S LOW threshold for BLUE tokens!");
+        node_handle.getParam("s_low_blue",blue_params.S.min);
+        ROS_ASSERT_MSG(node_handle.hasParam("v_low_blue"),"No V LOW threshold for BLUE tokens!");
+        node_handle.getParam("v_low_blue",blue_params.V.min);
+        ROS_ASSERT_MSG(node_handle.hasParam("h_high_blue"),"No H HIGH threshold for BLUE tokens!");
+        node_handle.getParam("h_high_blue",blue_params.H.max);
+        ROS_ASSERT_MSG(node_handle.hasParam("s_high_blue"),"No S HIGH threshold for BLUE tokens!");
+        node_handle.getParam("s_high_blue",blue_params.S.max);
+        ROS_ASSERT_MSG(node_handle.hasParam("v_high_blue"),"No V HIGH threshold for BLUE tokens!");
+        node_handle.getParam("v_high_blue",blue_params.V.max);
+        ROS_INFO_STREAM("Blue tokens in H=[" << blue_params.H.min<< ".." << blue_params.H.max <<
+                        "] S=[" << blue_params.S.min << ".." << blue_params.S.max <<
+                        "] V=[" << blue_params.V.min << ".." << blue_params.V.max<< "]");
 
-        ROS_ASSERT_MSG(this->nh_.hasParam("color_area_threshold"),"No color area threshold!");
-        this->nh_.getParam("color_area_threshold",this->TOKEN_AREA_THRESHOLD);
+        ROS_ASSERT_MSG(node_handle.hasParam("color_area_threshold"),"No color area threshold!");
+        node_handle.getParam("color_area_threshold",TOKEN_AREA_THRESHOLD);
     }
 
     ~BoardState()
@@ -150,7 +161,7 @@ public:
         }
 
         ttt_board_sensor::ttt_board msg_board;
-        msg_board.data.assign(undefined); // initialy the state of each cell is undefined
+        msg_board.data.assign(undefined); // initially the state of each cell is undefined
         msg_board.header.stamp = msg->header.stamp;
         msg_board.header.frame_id = msg->header.frame_id;
         short unsigned int counter=0;
@@ -158,14 +169,14 @@ public:
         {
             /* computing the red area in the cell */
             double red_cell_area = this->get_hsv_colored_area_in_cell(cv_ptr->image,cell,
-                                                                      cv::Scalar(H_LOWER_RED,S_LOWER_RED,V_LOWER_RED),
-                                                                      cv::Scalar(H_HIGHER_RED,S_HIGHER_RED,V_HIGHER_RED));
+                                                                      cv::Scalar(red_params.H.min,red_params.S.min,red_params.V.min),
+                                                                      cv::Scalar(red_params.H.max,red_params.S.max,red_params.V.max));
             ROS_DEBUG_STREAM("Red area of cell " << counter+1 << " = " << red_cell_area);
 
             /* computing the blue area in the cell */
             double blue_cell_area = this->get_hsv_colored_area_in_cell(cv_ptr->image,cell,
-                                                                      cv::Scalar(H_LOWER_BLUE,S_LOWER_BLUE,V_LOWER_BLUE),
-                                                                      cv::Scalar(H_HIGHER_BLUE,S_HIGHER_BLUE,V_HIGHER_BLUE));
+                                                                      cv::Scalar(blue_params.H.min,blue_params.S.min,blue_params.V.min),
+                                                                      cv::Scalar(blue_params.H.max,blue_params.S.max,blue_params.V.max));
             ROS_DEBUG_STREAM("Blue area of cell " << counter+1 << " = " << blue_cell_area);
 
             /* determining the state of the cell considering that two tokens can be heaped */
@@ -186,7 +197,7 @@ public:
         }
         if(last_msg_board!=msg_board)
         {
-            this->board_pub_.publish(msg_board);
+            board_publisher.publish(msg_board);
             last_msg_board=msg_board;
             ROS_DEBUG("NEW TTT BOARD STATE PUBLISHED");
         }
--- ttt_board_sensor/src/ttt_cells.h
+++ ttt_board_sensor/src/ttt_cells.h
@@ -1,14 +1,11 @@
 #ifndef TTT_CELLS_H
 #define TTT_CELLS_H
 
-/** Class used to manipulate general funtions on cells
-  *
-  *
-  */
+/** 
+ * Class used to manipulate general funtions on cells
+ *
+**/
 #include <ros/ros.h>
-//#include <ros/console.h>
-//#include <ros/assert.h>
-//#include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
