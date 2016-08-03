5a0f26665700890f839218ea2a3ae909963e17c6
Alessandro Roncone <alecive87@gmail.com>
2016-08-03 10:15:10 -0400

    Surfaced changes to the robot_interface API

--- src/tictactoe_brain/tictactoeBrain.h
+++ src/tictactoe_brain/tictactoeBrain.h
@@ -11,7 +11,7 @@
 #include "baxterTictactoe/tictactoe_utils.h"
 #include "baxterTictactoe/T_ThreadSafe.h"
 #include "tictactoe/PlaceTokenAction.h"
-#include "robot_interface/arm_controllers.h"
+#include "robot_interface/ttt_controllers.h"
 
 #include <pthread.h>
 
